9b30ad01720bc3786290d7db2c9b37bcdd2356fb
ariklin <ariklin@microsoft.com>
2023-06-18 20:48:18 +0300

    6

--- main.tf
+++ main.tf
@@ -112,7 +112,7 @@ module "irsa-ebs-csi" {
   version = "4.7.0"
 
   create_role                   = true
-  role_name                     = "AmazoneksTFEBSCSIRole-${module.explore-california-cluster.cluster_name}"
+  role_name                     = "AmazoneksTFEBSCSIRole-${module.eks.cluster_name}"
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
