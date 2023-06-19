9d2a09914056e5f389daaa30f7a860441461d1fb
ariklin <ariklin@microsoft.com>
2023-06-19 17:01:30 +0300

    13

--- main.tf
+++ main.tf
@@ -5,12 +5,16 @@ provider "aws" {
   region = var.region
 }
 
+locals {
+  cluster_name = "eks"
+}
+
 data "aws_eks_cluster" "cluster" {
-  name =  module.eks.cluster_id
+  cluster_name =  module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  cluster_name = module.eks.cluster_id
 }
 
 data "aws_availability_zones" "available" {
@@ -21,10 +25,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-locals {
-  cluster_name = "eks"
 }
 
 resource "random_string" "suffix" {
