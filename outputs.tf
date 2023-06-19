5c15d83637be00a85af53fbcef52a5bdc5feadf9
ariklin <ariklin@microsoft.com>
2023-06-19 17:33:37 +0300

    14

--- outputs.tf
+++ outputs.tf
@@ -20,3 +20,8 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
 }
+
+output "cluster_id" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_id
+}
